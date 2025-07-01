33ff3e789e0c76004fa5fa7fcb9a11d0c0f9541d
Sakib008 <mdshakib1902@gmail.com>
2025-07-02 00:10:47 +0530

    feat : dynamic action name for reducer

---
+++ frontend/src/utils/action.js
@@ -0,0 +1,7 @@
+const RESTAURANTS_ALL = "restaurant/all";
+const RESTAURANT_SINGLE = "restaurant/single";
+const RESTAURANT_UPDATE = "restaurant/update";
+const RESTAURANT_ADD = "restaurant/add";
+const RESTAURANT_DELETE = "restaurant/delete";
+
+export {RESTAURANTS_ALL,RESTAURANT_ADD,RESTAURANT_DELETE,RESTAURANT_SINGLE,RESTAURANT_UPDATE}
