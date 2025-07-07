0dd89af236b83955478288c70a6e2483b22aa6ca
Sakib008 <mdshakib1902@gmail.com>
2025-07-07 12:03:44 +0530

    fix : updated export and route link

--- backend/routes/restaurant.router.js
+++ backend/routes/restaurant.router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Router = express.Router()
 
 const {getAllRestaurant,createRestaurant,getSingleRestaurant,updateRestaurant,deleteRestaurant} = require("../controllers/restaurant")
-const { createDish } = require("../controllers/dish")
+const { createDish, deleteDish, editDish } = require("../controllers/dish")
 
 Router.get('/',getAllRestaurant);
 Router.post('/',createRestaurant)
@@ -12,5 +12,9 @@ Router.post('/:id', deleteRestaurant);
 
 // Add dish to restaurant menu
 Router.post('/:id/menu', createDish);
+// Delete dish from restaurant menu
+Router.post('/:id/menu/:menuId',deleteDish)
+// Edit dish to restaurant menu
+Router.post('/:id/menu/:menuId',editDish)
 
 module.exports = Router;
