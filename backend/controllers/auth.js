dd7cd328e36d98680fb6001e29df44bd9af6b590
Sakib008 <mdshakib1902@gmail.com>
2025-07-09 19:26:29 +0530

    fix : changed the verification function and some small bugs

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -5,34 +6,39 @@ const jwtSecret = process.env.JWT_SECRET;
 const createUser = async (req, res) => {
   try {
     const user = req.body;
-    const { username, email, password } = user;
-    const foundByUsername = await User.find({ username });
-    const foundByEmail = await User.find({ email });
+    const { username, email, password, firstname, lastname } = user;
+    const foundByUsername = await User.findOne({ username });
+    console.log("foundUser", foundByUsername);
+    const foundByEmail = await User.findOne({ email });
+
+    if (!username || !email || !password || !firstname || !lastname) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
 
     if (foundByUsername) {
-      res
+      return res
         .status(400)
-        .json({ error: "Username is already exist, choose another" });
+        .json({ error: "Username is already exist, choose another", username });
     }
     if (foundByEmail) {
-      res
+      return res
         .status(400)
-        .json({ error: "Email id is already exist choose another." });
+        .json({ error: "Email id is already exist choose another.", email });
     }
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
-    password = hashPassword;
-    const newUser = new User(user);
-    const token = await jwt.sign(username, jwtSecret, { expiresIn: "15d" });
+    const newUser = new User({ ...user, password: hashPassword });
+    const token = jwt.sign({ id: newUser._id, username }, jwtSecret, {
+      expiresIn: "15d",
+    });
+
     await newUser.save();
 
-    res
-      .status(201)
-      .json({
-        message: "User Created Successfully, Now Login",
-        user: newUser,
-        token,
-      });
+    res.status(201).json({
+      message: "User Created Successfully, Now Login",
+      user: newUser,
+      token,
+    });
   } catch (error) {
     if (error.name === "ValidationError") {
       const messages = Object.values(error.errors).map((err) => err.message);
@@ -44,7 +50,7 @@ const createUser = async (req, res) => {
         .status(400)
         .json({ error: "Email or username already exists" });
     }
-
+    console.error("error : ", error.message);
     res.status(500).json({ error: "Server error" });
   }
 };
@@ -52,27 +58,28 @@ const createUser = async (req, res) => {
 const logUser = async (req, res) => {
   try {
     const { username, password } = req.body;
-    const existUser = await User.find({ username });
-    if (existUser) {
-      const isMatchPassword = await bcrypt.compare(
-        password,
-        existUser.password
-      );
-      if (isMatchPassword) {
-        const token = await jwt.sign(username, jwtSecret, { expiresIn: "15d" });
-        res
-          .status(200)
-          .json({
-            message: "User Fetched Successfully",
-            user: existUser,
-            token,
-          });
-      }
-      res.status(400).json({ error: "password not matched" });
+    const existUser = await User.findOne({ username });
+    if (!existUser) {
+      return res
+        .status(400)
+        .json({ error: "User not exist with this credentials", username });
     }
-    res.status(400).json({ error: "Username is not exist" });
+    const isMatchPassword = await bcrypt.compare(password, existUser.password);
+    console.log("Mach password : ",isMatchPassword)
+    if (!isMatchPassword) {
+      return res.status(400).json({ error: "password not matched", password });
+    }
+    console.log("user : ",existUser)
+    const token = jwt.sign({id: existUser._id,username}, jwtSecret, { expiresIn: "15d" });
+    console.log("token : ",token)
+    res.status(200).json({
+      message: "User Fetched Successfully",
+      user: existUser,
+      token,
+    });
   } catch (error) {
-    res.status(500).json({ error: error, message: error.message });
+    console.error("Error : ",error.message)
+    return res.status(500).json({ error: error, message: error.message });
   }
 };
 
