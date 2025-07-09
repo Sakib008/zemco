2d343baa5d09a9fe8134db4fb494d6f3d99b64d5
Sakib008 <mdshakib1902@gmail.com>
2025-07-09 19:27:22 +0530

    feat : corrected the login router from get to post

--- backend/routes/auth.router.js
+++ backend/routes/auth.router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { logUser, createUser } = require('../controllers/auth')
 const Router = express.Router()
 
-Router.get('/login',logUser)
+Router.post('/login',logUser)
 Router.post('/register',createUser)
 
 module.exports = Router;
