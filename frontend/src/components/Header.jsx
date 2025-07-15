ca574c3f86edd26d1bda61daa728f1149c6b90b4
Sakib008 <mdshakib1902@gmail.com>
2025-07-15 14:03:03 +0530

    fix  : made sticky header with remove logout btn

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,32 +3,29 @@
 import { HandPlatter, Search, LogOut } from "lucide-react"
 import Link from "next/link";
 import { useAuth } from "@/context/authContext";
+import { useRef } from "react";
 
 const Header = () => {
   const { isAuthenticated, user, logoutUser } = useAuth();
+  const searchRef = useRef()
   
   return (
-    <header className="h-20 max-w-screen-2xl flex items-center justify-between mx-auto px-5 rounded-full m-3 border-2 border-gray-600 ">
+    <header className="sticky top-4 z-50 bg-white h-20 max-w-screen-2xl flex items-center justify-between mx-auto px-5 rounded-full m-3 border-2 border-gray-600 ">
       <Link href={'/'} className="w-12 h-12 bg-gradient-to-br flex items-center justify-center from-purple-600 via-pink-600 to-violet-800 rounded-3xl">
         <HandPlatter className="text-white  w-8 h-8"/>
       </Link>
-      <div className="flex p-2 px-3 rounded-full w-96  border-2 border-gray-500">
+      <div ref={searchRef}  className="flex items-center p-2 px-3 rounded-full w-96  border-2 border-gray-500">
         <input name="search" className="outline-none text-xl w-full" type="search"/>
         <label id="search " className="cursor-pointer"><Search/></label>
       </div>
       <div className="flex items-center gap-4">
+        <Link href={'/restaurant'} className="text-xl underline underline-offset-2">All Restaurant</Link>
         {isAuthenticated ? (
           <>
             <Link href={'/profile'} className="bg-violet-800 w-12 h-12 font-bold text-2xl text-white border-2 border-gray-600 rounded-full flex items-center justify-center">
               {user?.username ? user.username.charAt(0).toUpperCase() : 'U'}
             </Link>
-            <button 
-              onClick={logoutUser}
-              className="bg-red-600 w-12 h-12 text-white border-2 border-gray-600 rounded-full flex items-center justify-center hover:bg-red-700 transition-colors"
-              title="Logout"
-            >
-              <LogOut className="w-6 h-6" />
-            </button>
+           
           </>
         ) : (
           <Link href={'/login'} className="bg-violet-800 px-6 py-2 font-bold text-white border-2 border-gray-600 rounded-full hover:bg-violet-700 transition-colors">
