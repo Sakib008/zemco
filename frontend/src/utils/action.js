88470ad2e68a97e7eeacc4dda3db5aa14b6fac20
Sakib008 <mdshakib1902@gmail.com>
2025-07-16 23:04:10 +0530

    fix : action for filter

--- frontend/src/utils/action.js
+++ frontend/src/utils/action.js
@@ -4,5 +4,7 @@ const RESTAURANT_SINGLE = "restaurant/single";
 const RESTAURANT_UPDATE = "restaurant/update";
 const RESTAURANT_ADD = "restaurant/add";
 const RESTAURANT_DELETE = "restaurant/delete";
+const SET_FILTER = "restaurant/set_filter";
+const CLEAR_FILTER = "restaurant/clear_filter";
 
-export {RESTAURANTS_ALL,RESTAURANT_ADD,RESTAURANT_DELETE,RESTAURANT_SINGLE,RESTAURANT_UPDATE}
+export {RESTAURANTS_ALL,RESTAURANT_ADD,RESTAURANT_DELETE,RESTAURANT_SINGLE,RESTAURANT_UPDATE, SET_FILTER, CLEAR_FILTER}
