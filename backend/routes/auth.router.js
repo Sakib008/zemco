e059380883d6ec82b7774fd0c8c41ca5b5e7742a
Sakib008 <mdshakib1902@gmail.com>
2025-07-16 22:53:14 +0530

    feat : improved logout and cookie get

--- backend/routes/auth.router.js
+++ backend/routes/auth.router.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const { logUser, createUser } = require('../controllers/auth')
+const { logUser, createUser, logoutUser, getMe } = require('../controllers/auth')
+const authVerify = require('../middlewares/authVarify')
 const Router = express.Router()
 
 Router.post('/login',logUser)
 Router.post('/register',createUser)
+Router.get('/logout',logoutUser)
+Router.get('/me', authVerify, getMe)
 
 module.exports = Router;
