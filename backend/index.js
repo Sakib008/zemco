d11f1a1d8d61e0eb217a4a71d0eb5fcc26f7db55
Sakib008 <mdshakib1902@gmail.com>
2025-07-16 22:53:55 +0530

    fix : added cookie handle with parser and cors also image route

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,27 @@
 const cors = require('cors')
 const express = require('express')
 
+const cookieParser = require('cookie-parser')
 const initializeDatabase = require("./db/db.connect")
 const RestaurantRouter = require('./routes/restaurant.router')
 const AuthRouter = require('./routes/auth.router')
 const ReviewRouter = require('./routes/review.router')
+const ImageRouter = require("./routes/imageUpload.router")
 
 const app = express();
 app.use(express.json())
-app.use(cors());
+app.use(cookieParser())
+app.use(cors({
+  origin: 'http://localhost:3000', // Update this if your frontend runs elsewhere
+  credentials: true
+}));
 
 initializeDatabase()
 
 app.use('/api/restaurants',RestaurantRouter)
 app.use('/api/auth',AuthRouter)
 app.use('/api/reviews',ReviewRouter)
+app.use('/api/image',ImageRouter)
 
 app.get('/',((req,res)=>{
     res.send("Zomato Clone is Real : See------")
