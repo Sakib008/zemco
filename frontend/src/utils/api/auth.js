797dc2473b761063c007a67ad5e5e84d59b12606
Sakib008 <mdshakib1902@gmail.com>
2025-07-16 23:04:40 +0530

    fix : logout and get cookie function

--- frontend/src/utils/api/auth.js
+++ frontend/src/utils/api/auth.js
@@ -4,18 +4,37 @@ const API_BASE_URL = 'http://localhost:3001/api/auth';
 
 const login = async (userData)=>{
     try {
-        return await axios.post(`${API_BASE_URL}/login`,userData)
+        return await axios.post(`${API_BASE_URL}/login`,userData,{
+            withCredentials : true
+        })
     } catch (error) {
         throw error
     }
 }
 const signUp = async (userData)=>{
     try {
-        return await axios.post(`${API_BASE_URL}/register`,userData)
+        return await axios.post(`${API_BASE_URL}/register`,userData,{
+            withCredentials : true
+        })
     } catch (error) {
       
         throw error
     }
 }
+const logout = async()=>{
+    try{
+        return await axios.get(`${API_BASE_URL}/logout`, { withCredentials: true })
+    }catch(error){
+        throw error
+    }
+}
 
-export {login,signUp}
+const getMe = async () => {
+    try {
+        return await axios.get(`${API_BASE_URL}/me`, { withCredentials: true });
+    } catch (error) {
+        throw error;
+    }
+}
+
+export { login, signUp, logout, getMe };
