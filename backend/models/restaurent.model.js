8cfd1de7774ff85761400228ca3831e10734ee29
Sakib008 <mdshakib1902@gmail.com>
2025-07-16 22:54:48 +0530

    fix : update with image key

--- backend/models/restaurent.model.js
+++ backend/models/restaurent.model.js
@@ -5,14 +5,16 @@ const DishSchema = new mongoose.Schema({
   price: {type : Number,required : true},
   description: {type : String,required : true},
   isVeg : {type : Boolean, required : true},
+  image : {type : String},
   restaurantId : {type : mongoose.Schema.Types.ObjectId , ref : 'Restaurant'}
 });
 
 const RestaurantSchema = new mongoose.Schema({
   name:       { type: String, required: true, unique: true },
-  cuisine:    { type: String, required: true },
+  cuisine:    { type: String, required: true ,enum : ['indian',"chinese",'mughlai','south indian','punjabi','fusion','rajasthani','kerala','multi-cuisine'] },
   address:    { type: String, required: true },
   averageRating: { type: Number, default: 0 },
+  image : {type : String},
   menu:       [DishSchema],
   reviews:    [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }]
 }, { timestamps: true });
