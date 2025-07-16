2d5d36aaa21390f9d63161831677f4e6e7162bfd
Sakib008 <mdshakib1902@gmail.com>
2025-07-16 23:05:45 +0530

    fix: edit restaurant and imageAdd axios api function

--- frontend/src/utils/api/restaurant.js
+++ frontend/src/utils/api/restaurant.js
@@ -5,15 +5,27 @@ const allRestaurant = async () => {
   return await axios.get("/");
 };
 const addRestaurant = async (restaurantDetail) => {
-  return await axios.post("/", restaurantDetail);
+  return await axios.post("/", restaurantDetail,{
+  });
 };
 const singleRestaurant = async (id) => {
   return await axios.get(`/${id}`);
 };
 const editRestaurant = async (id, updateData) => {
+  if (updateData instanceof FormData) {
+    return await axios.post(`/${id}`, updateData, {
+      headers: { 'Content-Type': 'multipart/form-data' }
+    });
+  }
   return await axios.post(`/${id}`, updateData);
 };
 const removeRestaurant = async (id) => await axios.post(`/${id}`);
+
+const addImageRestaurant = async (formData)=>{
+  return await axios.post('http://localhost:3001/api/image/upload', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+    });
+}
 
 export {
   allRestaurant,
@@ -21,4 +33,5 @@ export {
   singleRestaurant,
   editRestaurant,
   removeRestaurant,
+  addImageRestaurant
 };
