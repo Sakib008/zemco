8e39c0c5c05e42e652a622116e6a828cbefd74e3
Sakib008 <mdshakib1902@gmail.com>
2025-07-16 22:59:39 +0530

    fix : link to all restaurant page

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,25 +2,39 @@
 import Header from "@/components/Header";
 import RestaurantCard from "@/components/RestaurantCard";
 import { useRestaurant } from "@/context/restaurantContext";
+import { ArrowRight } from "lucide-react";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
-  const { state } = useRestaurant();
-  console.log("Restaurants List : ", state.restaurants);
-
+  const { state, getAllRestaurant } = useRestaurant();
+  useEffect(() => {
+    getAllRestaurant();
+  }, [state.restaursts]);
+  console.log("All State Restaurant : ",state.restaurants)
   return (
     <>
       <Header />
-      <main className="max-w-screen-2xl mx-auto text-center border-2 min-h-screen border-purple-800">
-        <section className="w-full h-80 mb-5 font-bold text-8xl bg-gradient-to-tr from-purple-500 via-pink-600 to-violet-800 flex justify-center items-center rounded-3xl text-white">
+      <main className="max-w-screen-2xl mx-auto text-center min-h-screen ">
+        <section className="w-full h-80 mb-5 font-bold text-3xl md:text-4xl lg:text-8xl bg-gradient-to-tr from-purple-500 via-pink-600 to-violet-800 flex justify-center items-center rounded-3xl text-white">
           <h1>Best Restaurants of Year 2025</h1>
         </section>
+        <div className="flex justify-end text-right items-center">
+        <Link
+          href={"/restaurant"}
+          className="bg-blue-700 text-white flex text-2xl rounded-2xl p-2 items-center justify-center "
+          >
+              <ArrowRight />
+            View All Restaurants
+        </Link>
+            </div>
+          
         <section className="flex flex-wrap gap-10 justify-center">
           {state.restaurants.length === 0 ? (
             <p className="text-8xl text-center m-10 font-bold">Loading...</p>
           ) : (
             state.restaurants.map((restaurant) => (
-              <Link href={`restaurant/${restaurant._id}`}  key={restaurant._id}>
+              <Link href={`restaurant/${restaurant._id}`} key={restaurant._id}>
                 <RestaurantCard restaurant={restaurant} />
               </Link>
             ))
