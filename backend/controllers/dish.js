6980cf1f350be2481bdd2e0482562267bb534a58
Sakib008 <mdshakib1902@gmail.com>
2025-07-21 20:33:40 +0530

    fix : updated menu , edit menu and delete dish

--- backend/controllers/dish.js
+++ backend/controllers/dish.js
@@ -2,6 +2,14 @@ const Restaurant = require("../models/restaurent.model");
 const createDish = async (req, res) => {
   try {
     const { id } = req.params;
+    const isAdmin = req.user.isAdmin;
+    if (!isAdmin) {
+      return res
+        .status(404)
+        .json({
+          message: "You are not authorized to do this.(Only for Admin)",
+        });
+    }
     const { name, price, description, isVeg } = req.body;
     if (!name || !price || !description || isVeg === undefined) {
       return res.status(400).json({
@@ -17,6 +25,7 @@ const createDish = async (req, res) => {
       price: Number(price),
       description,
       isVeg: Boolean(isVeg),
+      image : req.file ? req.file.path : undefined,
     };
     foundRestaurant.menu.push(newDish);
     await foundRestaurant.save();
@@ -28,7 +37,7 @@ const createDish = async (req, res) => {
   } catch (error) {
     console.error("Error creating dish : ", error.message);
     res.status(500).json({
-      message: "Internal Server Error",
+      message: "Internal Server Error", error: error.message || error,
     });
   }
 };
@@ -38,60 +47,81 @@ const editDish = async (req, res) => {
     const { id } = req.params;
     const { menuId } = req.params;
     const { name, price, description, isVeg } = req.body;
+    const isAdmin = req.user.isAdmin;
+    if (!isAdmin) {
+      return res
+        .status(404)
+        .json({
+          message: "You are not authorized to do this.(Only for Admin)",
+        });
+    }
+
     const foundRestaurant = await Restaurant.findById(id);
     const foundDish = await Restaurant.menu.findById(menuId);
 
     if (!foundRestaurant) {
-     return res.status(400).json({ message: "No restaurent is found with this Id" });
+      return res
+        .status(400)
+        .json({ message: "No restaurent is found with this Id" });
     }
 
     if (!foundDish) {
-     return res.status(400).json({ message: "No dish found with this Id" });
+      return res.status(400).json({ message: "No dish found with this Id" });
     }
 
     if (!name || !price || !description || isVeg === undefined) {
-    return  res.status(400).json({ message: "All Fields are required" });
+      return res.status(400).json({ message: "All Fields are required" });
     }
     const newDish = {
       name,
       price: Number(price),
       description,
       isVeg: Boolean(isVeg),
+      image: req.file ? req.file.path : foundDish.image,
     };
     const editDish = foundRestaurant.menu.findByIdAndUpdate(menuId, newDish);
-    await foundRestaurant.save()
+    await foundRestaurant.save();
 
-    res
-      .status(201)
-      .json({
-        message: "dish updated Successfully",
-        dish: editDish,
-        restaurant: foundRestaurant.name,
-      });
+    res.status(201).json({
+      message: "dish updated Successfully",
+      dish: editDish,
+      restaurant: foundRestaurant.name,
+    });
   } catch (error) {
-    console.error(error)
-    res.status(500).json({error : 'Internal Server error'})
+    console.error(error);
+    res.status(500).json({ message : "Internal Server error", error: error.message || error });
   }
 };
 
-const deleteDish = async(req,res)=>{
+const deleteDish = async (req, res) => {
   try {
-    const {id,menuId} = req.params;
-
-    const foundRestaurant = await Restaurant.findById(id);
-    if(!foundRestaurant){
-     return res.status(400).json({error : 'Restaurant is not found with this id'});
-    } 
-    const foundMenu = foundRestaurant.menu.findById(menuId)
-    if(!foundMenu){
-     return res.status(400).json({error : 'No Dish found with this DishId'})
+    const { id, menuId } = req.params;
+    const isAdmin = req.user.isAdmin;
+    if (!isAdmin) {
+      return res
+        .status(404)
+        .json({
+          message: "You are not authorized to do this.(Only for Admin)",
+        });
     }
-   foundMenu.remove()
-    await foundRestaurant.save()
+    const foundRestaurant = await Restaurant.findById(id);
+    if (!foundRestaurant) {
+      return res
+        .status(400)
+        .json({ error: "Restaurant is not found with this id" });
+    }
+    const foundMenu = foundRestaurant.menu.findById(menuId);
+    if (!foundMenu) {
+      return res.status(400).json({ error: "No Dish found with this DishId" });
+    }
+    foundMenu.remove();
+    await foundRestaurant.save();
   } catch (error) {
-    console.error(error)
-    res.status(500).json({error : error.message, message : "Internal Server Error"})
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: error.message, message: "Internal Server Error" });
   }
-}
+};
 
-module.exports = {createDish,deleteDish,editDish}
+module.exports = { createDish, deleteDish, editDish };
