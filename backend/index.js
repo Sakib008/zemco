cacc7e6214a23a6404e5b45edcf257c30b61d919
Sakib008 <mdshakib1902@gmail.com>
2025-09-16 10:22:01 +0530

    fix : cors issue

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,29 @@
-const cors = require('cors')
-const express = require('express')
+const cors = require("cors");
+const express = require("express");
 
-const cookieParser = require('cookie-parser')
-const initializeDatabase = require("./db/db.connect")
-const RestaurantRouter = require('./routes/restaurant.router')
-const AuthRouter = require('./routes/auth.router')
-const ReviewRouter = require('./routes/review.router')
-const ImageRouter = require("./routes/imageUpload.router")
+const cookieParser = require("cookie-parser");
+const initializeDatabase = require("./db/db.connect");
+const RestaurantRouter = require("./routes/restaurant.router");
+const AuthRouter = require("./routes/auth.router");
+const ReviewRouter = require("./routes/review.router");
+const ImageRouter = require("./routes/imageUpload.router");
+const { get } = require("mongoose");
 
 const app = express();
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-  origin: ["https://zemco.vercel.app/","http://localhost:3000/"],
-  credentials: true
-}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
-initializeDatabase()
+initializeDatabase();
 
-app.use('/api/restaurants',RestaurantRouter)
-app.use('/api/auth',AuthRouter)
-app.use('/api/reviews',ReviewRouter)
-app.use('/api/image',ImageRouter)
+app.use("/api/restaurants", RestaurantRouter);
+app.use("/api/auth", AuthRouter);
+app.use("/api/reviews", ReviewRouter);
+app.use("/api/image", ImageRouter);
 
-app.get('/',((req,res)=>{
-    res.send("Zomato Clone is Real : See------")
-}))
-
+app.get("/", (req, res) => {
+  res.send("Zomato Clone is Real : See------");
+});
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT,()=>console.log("Server is Running on PORT : ",PORT))
+app.listen(PORT, () => console.log("Server is Running on PORT : ", PORT));
