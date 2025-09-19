038ef633be22bdc0ad71b6b2a46c03a16afc3ac7
Sakib008 <mdshakib1902@gmail.com>
2025-09-19 12:38:49 +0530

    fix : fetching api of restaurant

--- frontend/src/utils/api/restaurant.js
+++ frontend/src/utils/api/restaurant.js
@@ -5,7 +5,7 @@ const axios = require("./axiosInstance");
 const allRestaurant = async () => {
   try {
     console.log("Making API call to:", axios.defaults.baseURL);
-    const response = await axios.get("/");
+    const response = await axios.get("/restaurants");
     console.log("API Response:", response);
     return response;
   } catch (error) {
@@ -16,38 +16,38 @@ const allRestaurant = async () => {
 };
 const addRestaurant = async (restaurantDetail) => {
   if(restaurantDetail instanceof FormData){
-      return await axios.post("/", restaurantDetail, {
+      return await axios.post("/restaurants", restaurantDetail, {
       headers: { "Content-Type": "multipart/form-data" },
       withCredentials: true,
     });
   }else{
-    return await axios.post('/',restaurantDetail,{
+    return await axios.post('/restaurants',restaurantDetail,{
       withCredentials : true
     })
   }
 };
 const singleRestaurant = async (id) => {
-  return await axios.get(`/${id}`);
+  return await axios.get(`/restaurants/${id}`);
 };
 const editRestaurant = async (id, updateData) => {
   if (updateData instanceof FormData) {
-    return await axios.post(`/${id}`, updateData, {
+    return await axios.post(`/restaurants/${id}`, updateData, {
       headers: { "Content-Type": "multipart/form-data" },
       withCredentials : true
     });
   }
-  return await axios.post(`/${id}`, updateData,{
+  return await axios.post(`/restaurants/${id}`, updateData,{
     withCredentials  : true
   });
 };
 const removeRestaurant = async (id) =>
-  await axios.post(`/${id}`, {
+await axios.post(`/restaurants/${id}`, {
     headers: { "Content-Type": "multipart/form-data" },
     withCredentials: true,
   });
 
 const addImageRestaurant = async (formData) => {
-  return await axios.post("http://localhost:3001/api/image/upload", formData, {
+  return await axios.post(`/image/upload`, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
 };
