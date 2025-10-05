8c5661e9ddd9a51cc545408627eb09ba63fc491c
Sakib008 <mdshakib1902@gmail.com>
2025-10-05 23:48:25 +0530

    feat : admin restaurant page

--- frontend/src/app/profile/components/dashboard/AdminRestaurents.jsx
+++ frontend/src/app/profile/components/dashboard/AdminRestaurents.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
-
+import  { useEffect, useState } from 'react'
+import Restaurant from './components/adRestaurant'
+import { useRestaurant } from '@/context/restaurantContext'
 const AdminRestaurants = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  const { getAllRestaurant,state } = useRestaurant();
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchRestaurants = async () => {
+      try {
+        if(state.restaurants.length===0){
+     getAllRestaurant();
+        }
+      } catch (error) {
+        setError(error.response.data.message || error.message || "Failed to fetch restaurants");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchRestaurants();
+  },
+  []);
+
+  if (isLoading) {
+    return (
+      <div className="text-center w-full h-full  flex justify-center items-center text-2xl font-bold">
+        <div className="size-20 rounded-full bg-transparent border-4 border-r-0 border-r-transparent border-purple-800 animate-spin"></div>
+      </div>
+    );
+  }
+  if(error){
+    return <div className='text-center'>{error}</div>
+      
+  }
 
   return (
-    <div>AdminRestaurants</div>
+    <div>
+      {state.restaurants.map((restaurant)=>(
+        <div key={restaurant._id}>
+          <Restaurant restaurant={restaurant} />
+        </div>
+      ))} 
+    </div>
   )
 }
 
