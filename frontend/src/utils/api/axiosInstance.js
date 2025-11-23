f9157ee24a0725293a74ef761cb316ca856081ef
Sakib008 <mdshakib1902@gmail.com>
2025-11-23 13:38:03 +0530

    fix : added lambda url

--- frontend/src/utils/api/axiosInstance.js
+++ frontend/src/utils/api/axiosInstance.js
@@ -1,8 +1,9 @@
 const { default: axios } = require("axios");
 const localUrl = "http://localhost:3001/api/"
+const lambdaUrl = "https://kp4fka6atwn7fstjnshyg3ypiq0wjisi.lambda-url.us-east-1.on.aws/api"
 const renderUrl ='https://zemco-backend.onrender.com/api'
 const axiosInstance = axios.create({
-    baseURL : localUrl,
+    baseURL : lambdaUrl,
     headers : {
         "Content-Type" : "application/json"
     }
